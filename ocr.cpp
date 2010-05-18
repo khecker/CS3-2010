de4de89f061f152b34b39f11f0d6b6af216a9f62
Kelley Hecker <khecker@caltech.edu>
2010-05-18 15:03:56 -0700

    OCR, problem loading images?

--- ocr.cpp
+++ ocr.cpp
@@ -5,13 +5,14 @@
 #include <iostream.h>
 #include <fstream.h>
 
-int train_samples = 500;
-int test_samples = 500;
+int train_samples = 5;
+int test_samples = 50;
 int num_classes = 10;
 int max_k = 32;
 int size = 28;
+IplImage zeroimg;
 CvMat* trainClasses = cvCreateMat(train_samples * num_classes, 1, CV_32FC1);
-CvMat* trainData = cvCreateMat(train_samples * num_classes, 2, CV_32FC1);
+CvMat* trainData = cvCreateMat(train_samples * num_classes, size*size, CV_32FC1);
 
 /* 
  * This function takes an image and to pointers to ints and sets
@@ -157,10 +158,8 @@ void getData() {
      // Read the image files data0 - data9.
      for(i = 0; i < num_classes; i++)
      {
-           ifstream read_file;
-
            sprintf(filename, "images/data%i", i);
-           read_file.open(filename);
+           ifstream read_file(filename);
            
            if(!read_file) // file couldn't be opened
                  printf("Error: file %s could not be opened\n", filename);
@@ -172,6 +171,8 @@ void getData() {
                  // Create a new image of size 28x28. Each pixel is 8 bits 
                  // with one channel.
                  source = cvCreateImage(cvSize(size, size), IPL_DEPTH_8U,1);
+                 
+                 //for(x = 0; x < 1*size*size; x++) {read_file >> pixel;}
      
                  for(x = 0; x < size; x++)
                  {
@@ -183,34 +184,114 @@ void getData() {
                              read_file >> pixel;
                              value = cvRealScalar((int)pixel);
                              cvSet2D(source, x, y, value);
+                             if((int)pixel != 0)
+                                           printf("  ");
+                             else
+                                 printf("0 ");
                        }
+                       printf("\n");
                  }
                  
-                 read_file.close();
-                 
+                 //while(1);
                  // Process the image
                  processed = preprocessing(source, size, size);
                  
-                 // Set classes.
-                 cvGetRow(trainClasses, &row, i*train_samples + j);
-                 cvSet(&row, cvRealScalar(i));
+                 /*
+                 // Testing the preprocessing code.
+                 for(x = 0; x < size; x++)
+                 {
+                       for(y = 0; y < size; y++)
+                       {
+                             pixel2 = processed.imageData[size*x + y];
+                             if((int)pixel2 != 0)
+                                  printf("  ");
+                             else
+                                  printf("%i ", 0);
+                       }
+                       printf("\n");
+                 }
                  
-                 // Set data.
-                 cvGetRow(trainData, &row, i*train_samples + j);
+                 while(1);
+                 */
                  
                  // Convert our 8-bit image into a 32-float image.
                  IplImage* floatImage = cvCreateImage(cvSize(size, size), 
                       IPL_DEPTH_32F, 1);
-                 cvConvertScale(&processed, floatImage, 0.0039215, 0);
+                 cvConvertScale(&processed, floatImage, 1, 0);//0.0039215, 0);
                  
+                 if(i == 5 && j == 0) 
+                      zeroimg = processed;
+                 // Get reference to the i*train_samples + j row, and set
+                 // it to i.
+                 cvGetRow(trainClasses, &row, i*train_samples + j);
+                 cvSet(&row, cvRealScalar(i));
+                 
+                 // Get reference to the i*train_samples + j row in trainData.
+                 cvGetRow(trainData, &row, i*train_samples + j);
+                 //printf("row rows: %i, cols: %i\n", row.rows, row.cols);
                  // Moves rectangle from source into data
                  cvGetSubRect(floatImage, &data, cvRect(0, 0, size, size));
-
+                 //printf("data rows: %i, cols: %i\n", data.rows, data.cols);
                  CvMat row_header, *row1;
-                 row1 = cvReshape(&data, &row_header, 0, 1);
-                 //cvCopy(row1, &row);
+                 row1 = cvReshape(&data, &row_header, 0, 1);//(size*size/2));
+                 //printf("row1 rows: %i, cols: %i\n", row1->rows, row1->cols);
+                 cvCopy(row1, &row);
            }
+           read_file.close();
      }
+}
+
+float find_nearest(CvMat* img, int max_k) {
+      float neighbors[max_k][2];
+      float result;
+      int x, y;
+      double myval, otherval, clas, temp;
+      float temp_dist, temp_class;
+      float dist = 0;
+      float top = 0;
+      float bottom = 0;
+      
+      for(x = 0; x < max_k; x++)
+      {
+            neighbors[x][0] = -1;
+            neighbors[x][1] = -1;
+      }
+      
+      for(x = 0; x < train_samples*num_classes; x++)
+      {
+            clas = cvGet1D(trainClasses, x).val[0];
+            for(y = 0; y < size*size; y++)
+            {
+                  otherval = cvGet2D(trainData, x, y).val[0];
+                  myval = cvGet1D(img, y).val[0];
+                  temp = otherval - myval;
+                  if(temp < 0)
+                        temp = -1 * temp;
+                  dist = dist + temp;
+            }
+            for(y = 0; y < max_k; y++)
+            {
+                  if(neighbors[y][1] == -1 || neighbors[y][1] > dist)
+                  {
+                        temp_dist = neighbors[y][1];
+                        temp_class = (int)neighbors[y][0];
+                        neighbors[y][1] = dist;
+                        neighbors[y][0] = clas;
+                        dist = temp_dist;
+                        clas = temp_class;
+                  }
+            }
+            dist = 0;
+      }
+      for(x = 0; x < max_k; x++)
+      {
+            printf("class: %i, dist: %f\n", (int)neighbors[x][0], neighbors[x][1]);
+            top = top + neighbors[x][0] * (1 / (neighbors[x][1] * neighbors[x][1]));
+            bottom = bottom + (1 / (neighbors[x][1] * neighbors[x][1]));
+      }
+      
+      result = top/bottom;
+      return result;
 }
 
 // Takes an image file and uses CvKNearest.find_nearest to convert the number 
@@ -226,7 +307,7 @@ float classify(IplImage* img, CvKNearest* knn) {
       // Convert our 8-bit image into a 32-float image.
       IplImage* floatImage = cvCreateImage(cvSize(size, size), 
            IPL_DEPTH_32F, 1);
-      cvConvertScale(&processed, floatImage, 0.0039215, 0);
+      cvConvertScale(&processed, floatImage, 1, 0);//0.0039215, 0);
                  
       // Moves rectange from source into data
       cvGetSubRect(floatImage, &data, cvRect(0, 0, size, size));
@@ -234,10 +315,11 @@ float classify(IplImage* img, CvKNearest* knn) {
       //printf("data rows: %i, data cols: %i\n", &data->rows, &data->cols);
 
       CvMat row_header, *row1, *row2;
-      row1 = cvReshape(&data, &row_header, 0, (28*28/2));
+      row1 = cvReshape(&data, &row_header, 0, 1);
       
       // Get the float corresponding to this image.
       result = knn->find_nearest(row1, max_k, 0, 0, 0, 0);
+      //result = find_nearest(row1, max_k);
       return result;
 }
 
@@ -265,7 +347,7 @@ void test(CvKNearest* knn) {
            else
                  printf("Reading file %s\n", filename);
            
-           for(j = 0; j < train_samples; j++)
+           for(j = 0; j < train_samples*size*size; j++)
            {
                  read_file >> pixel;
            }
@@ -288,25 +370,23 @@ void test(CvKNearest* knn) {
                              cvSet2D(source, x, y, value);
                        }
                  }
-                 
-                 read_file.close();
-                 
-                 // Process the image
-                 processed = preprocessing(source, size, size);
-                 
-                 result = classify(&processed, knn);
+
+                 result = classify(source, knn);
                  
                  printf("Reading the %ith image in this file, classified as: %i\n", j, (int)result);
                  if((int)result != i)
                        errors++;
            }
+           read_file.close();
      }
-     errorPercent = 100 * (float)errors / (float)test_samples;
+     errorPercent = 100 * (float)errors / (float)(test_samples*num_classes);
      printf("Error percentage: %4.2f\n", errorPercent);
 }
       
 int main( int argc, char** argv ) {
-    getData();
-    CvKNearest* knn = new CvKNearest(trainData, trainClasses, 0, false, max_k);
-    test(knn);
+    getData();  
+    //CvKNearest* knn = new CvKNearest(trainData, trainClasses, 0, false, max_k);  
+    //test(knn);
+    //printf("result: %i\n", (int)classify(&zeroimg, knn));
+    while(1);
 }
